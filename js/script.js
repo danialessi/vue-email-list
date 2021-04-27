30b01ef919bfe4b418f6f24510d849c29996fe65
danialessi <daniele.alessi@hotmail.it>
2021-04-27 16:25:06 +0200

    stampata lista email

--- js/script.js
+++ js/script.js
@@ -1,7 +1,28 @@
+// Attraverso l'apposita API di Boolean https://flynn.boolean.careers/exercises/api/random/mail generare 10 indirizzi email e stamparli in pagina all'interno di una lista.
+// Bonus
+// Far comparire gli indirizzi email solamente quando sono stati tutti generati.
+
 var app = new Vue (
     {
         el:"#root",
         data: {
+            listaEmail: []
+        },
+        methods: {
+
+        },
+        mounted() {
+            for ( i = 0; i < 10; i++ ) {
+                axios
+                .get('https://flynn.boolean.careers/exercises/api/random/mail')
+                .then((response) => {
+                    // salvo in una variabile il risultato della chiamata al server (sarà un oggetto)
+                        const result = response.data;
+                        const randomEmail = result.response;
+                        this.listaEmail.push(randomEmail);
+                });
+            }
+            console.log(this.listaEmail);
             
         }
     }
